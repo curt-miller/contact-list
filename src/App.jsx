c98bbf4f2789dfe1e03dcc1b6ab0124dead0a79b
Curt Miller <millercw3@gmail.com>
2024-12-04 11:58:45 -0500

    table template layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
 
   return (
     <>
-    
+<ContactList/>
     </>
   )
   
